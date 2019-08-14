371796792bdb0fb64278ba3009a5054c6caedbf3
Ola Przytula <aleksandra.przytula1995@gmail.com>
2019-08-14 19:36:37 +0200

    poruszanie-rook

--- src/Game/board.js
+++ src/Game/board.js
@@ -1,5 +1,6 @@
 import Pawn from "./pieces/pawn";
-import Knight from "./pieces/knight"
+import Knight from "./pieces/knight";
+import Rook from "./pieces/rook";
 
 // klasa inicjująca tablicę, gdzie będą zapisywane pozycje bierek
 
@@ -29,6 +30,8 @@ board.init();
 
 // tutaj tworzycie nowe obiekty waszych bierek i od razu umieszczacie je na szachownicy
 
+// KNIGHT //
+
 let knight_w1 = new Knight(7, 1, "white");
 board[knight_w1._x][knight_w1._y] = knight_w1;
 let knight_w2 = new Knight(7, 6, "white");
@@ -37,6 +40,19 @@ let knight_b1 = new Knight(0, 1, "black");
 board[knight_b1._x][knight_b1._y] = knight_b1;
 let knight_b2 = new Knight(0, 6, "black");
 board[knight_b2._x][knight_b2._y] = knight_b2;
+
+// ROOK //
+
+let rook_w1 = new Rook(0, 7, "white");
+board[rook_w1._x][rook_w1._y] = rook_w1;
+let rook_w2 = new Rook(7, 7, "white");
+board[rook_w2._x][rook_w2._y] = rook_w2;
+let rook_b1 = new Rook(0, 0, "black");
+board[rook_b1._x][rook_b1._y] = rook_b1;
+let rook_b2 = new Rook(7, 0, "black");
+board[rook_b2._x][rook_b2._y] = rook_b2;
+
+
 
 console.log(board); // służy do podejrzenia tablicy w konsoli
 
